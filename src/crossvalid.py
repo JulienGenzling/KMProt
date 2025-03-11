bd519e88b0d026bc84ceb0214685b4e8e9bb8af4
Julien Genzling <julien.genzling@jaguar.polytechnique.fr>
2025-03-11 21:47:24 +0100

    clean commentaires

--- src/crossvalid.py
+++ src/crossvalid.py
@@ -51,90 +51,13 @@ if __name__ == "__main__":
     from src.kernel import MultiSpectrumKernel, MismatchKernel, WeightedSumKernel
     from src.fitter import SVM
 
-    # all_acc = []
-    # for i in range(10):
-    #     dataset = Dataset(k=0)
-    #     # dataset.sequences = np.array(["ATCT", "ATTT", "CGTA", "CTCT", "CTTC"])
-    #     # dataset.labels = np.array([0, 1, 1, 1, 0])
-    #     params = {'name': 'spectrum', 'kmin': 5, 'kmax': 9}
-    #     kernel = MultiSpectrumKernel(dataset, **params, verbose=True)
-    #     fitter = SVM(C=1, tol=1e-4)
-    #     cross_valid = CrossValid(fitter, dataset, kernel, k=5)
-    #     results, acc = cross_valid.fit()
-    #     print(acc)
-    #     all_acc.append(acc)
-    # print(np.mean(all_acc), " +- ", np.std(all_acc))
-
     all_acc = []
     for i in range(10):
         dataset = Dataset(k=0)
-        # dataset.sequences = np.array(["ATCT", "ATTT", "CGTA", "CTCT", "CTTC"])
-        # dataset.labels = np.array([0, 1, 1, 1, 0])
-        params = [
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 9,
-                "weight": 0.10064644404805882
-            },
-            {
-                "name": "mismatch",
-                "k": 11,
-                "m": 2,
-                "weight": 0.10044535231872262
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 12,
-                "weight": 0.10034495717234335
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 14,
-                "weight": 0.10004437340191975
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 15,
-                "weight": 0.09999436371868145
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 13,
-                "weight": 0.09994437903403464
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 16,
-                "weight": 0.09979457484671235
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 11,
-                "weight": 0.0995951851531757
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 18,
-                "weight": 0.0995951851531757
-            },
-            {
-                "name": "spectrum",
-                "kmin": 5,
-                "kmax": 20,
-                "weight": 0.0995951851531757
-            }
-        ]
-        kernel = WeightedSumKernel(dataset, kernel_params_list=params, verbose=True)
+        params = {'name': 'spectrum', 'kmin': 5, 'kmax': 9}
+        kernel = MultiSpectrumKernel(dataset, **params, verbose=True)
         fitter = SVM(C=1, tol=1e-4)
-        cross_valid = CrossValid(fitter, dataset, kernel, weight=True, k=5)
+        cross_valid = CrossValid(fitter, dataset, kernel, k=5)
         results, acc = cross_valid.fit()
         print(acc)
         all_acc.append(acc)
